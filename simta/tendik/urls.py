f71dce47b7ccb96c80e677fad85dc99941f1fffc
rizal-haw <rizalhidayat006@gmail.com>
2021-11-23 16:52:38 +0700

    Update URLs

--- simta/tendik/urls.py
+++ simta/tendik/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from tendik.views import dashboardView, pembimbingView
+from tendik.views import dashboardView, pembimbingView, mahasiswaView, pengajuanJudulView
 
 urlpatterns = [
     path('', dashboardView.as_view(), name='index'),
     path('pembimbing', pembimbingView.as_view(), name='pembimbing'),
+    path('mahasiswa', mahasiswaView.as_view(), name='mahasiswa'),
+    path('pengajuan-judul', pengajuanJudulView.as_view(), name='mahasiswa'),
 ]
