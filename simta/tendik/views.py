5bc56b6859100549868981ed99cb71003d3a034d
rizal-haw <rizalhidayat006@gmail.com>
2021-11-23 16:53:08 +0700

    Menambahkan fungsi di Views

--- simta/tendik/views.py
+++ simta/tendik/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
+
 class dashboardView(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'tendik/index.html')
@@ -8,3 +9,11 @@ class dashboardView(View):
 class pembimbingView(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'tendik/pembimbing.html')
+
+class mahasiswaView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'tendik/mahasiswa.html')
+
+class pengajuanJudulView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'tendik/pengajuanjudul.html')
