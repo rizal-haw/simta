c1e0ca451cd6f39e366c0bdf2b5ea3d7555bee16
rizal-haw <rizalhidayat006@gmail.com>
2021-11-26 09:11:23 +0700

    Update urls root

--- simta/simta/urls.py
+++ simta/simta/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('', include('landing.urls')),
     path('tendik/', include('tendik.urls')),
     path('mhs/', include('mahasiswa.urls')),
+    path('pembimbing/', include('pembimbing.urls')),
     path('admin/', admin.site.urls),
 ]
