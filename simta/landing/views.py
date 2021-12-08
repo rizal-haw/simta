b39cc2c260bb1d97143f79f82a5555b02fcb5de1
rizal-haw <rizalhidayat006@gmail.com>
2021-12-08 11:56:13 +0700

    mengerakan auth

--- simta/landing/views.py
+++ simta/landing/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views import View
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 class Index(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'landing/index.html')
+
+def login_user(request):
+    return render(request, 'authenticate/login.html')
